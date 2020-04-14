4f17730ce6bf95a0d5bcee47612d8705813739f7
m4dn355 <xm4dn355x@gmail.com>
2020-04-14 22:13:40 +0500

    added skeleton of gestapo and big_brother apps

---
+++ papers/big_brother/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BigBrotherConfig(AppConfig):
+    name = 'big_brother'
---
+++ papers/gestapo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestapoConfig(AppConfig):
+    name = 'gestapo'
