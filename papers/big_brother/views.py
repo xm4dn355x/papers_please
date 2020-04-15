3ce9044c4082cf3df45fc2dfdecdb851ea783b0b
m4dn355 <xm4dn355x@gmail.com>
2020-04-15 21:09:23 +0500

    added logout page and login permission for big_brother app

--- papers/big_brother/views.py
+++ papers/big_brother/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 
 
+@login_required
 def bb_index(request):
     template = loader.get_template('big_brother/index.html')
     context = {}
