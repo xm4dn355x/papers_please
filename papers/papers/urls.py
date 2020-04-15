762295ab1c986a14625ca23b4574d7f151043545
m4dn355 <xm4dn355x@gmail.com>
2020-04-15 19:48:12 +0500

    added login page

--- papers/papers/urls.py
+++ papers/papers/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('slaves.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
