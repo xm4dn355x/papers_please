3289357ca45180ba6fc0b51a3d47ce615dbdd93d
m4dn355 <xm4dn355x@gmail.com>
2020-04-20 14:44:58 +0500

    added accepted/declined busines logic and some fixes for old funcs

--- papers/big_brother/views.py
+++ papers/big_brother/views.py
@@ -12,8 +12,7 @@ from .models import *
 @login_required
 @csrf_protect
 def bb_index(request):
-    # TODO: Прописать логику обработки POST Запроса
-    if request.method == 'POST':
+    if request.method == 'POST': # TODO: При изменении пропуска сначала ищется существуюший пропуск, а потом изменяется
         query = request.POST
         user = query.get('user')
         req_id = query.get('req_id')
@@ -27,14 +26,20 @@ def bb_index(request):
             status = 'СТАТУС ПРОПУСКА НЕОПРЕДЕЛЕН'
         processed_car_request = Car_requests.objects.get(id=req_id)
         processed_car_request.status = decision
-        car_pass = Car_passes.objects.create(
-            company_id=processed_car_request.company_id,
-            car_id=processed_car_request.car_id,
-            req_id=processed_car_request,
-            moderator=user,
-            #pass_time= ,# TODO: Добавить срок действия пропуска
-            status=status
-        )
+        try:
+            car_pass = Car_passes.objects.get(car_id=processed_car_request.car_id)
+            car_pass.status = status
+            car_pass.moderator_comment = comment
+        except Car_passes.DoesNotExist:
+            car_pass = Car_passes.objects.create(
+                company_id=processed_car_request.company_id,
+                car_id=processed_car_request.car_id,
+                req_id=processed_car_request,
+                moderator=user,
+                moderator_comment=comment,
+                #pass_time= ,# TODO: Добавить срок действия пропуска
+                status=status,
+            )
         processed_car_request.save()
         car_pass.save()
         car_requests = Car_requests.objects.filter(status='В ожидании проверки')
@@ -43,7 +48,7 @@ def bb_index(request):
                    'user': user,
                    'req_id': req_id,
                    'decision': decision,
-                   'comment': comment, }
+                   'comment': comment,}
         return HttpResponse(template.render(context, request))
     if request.method == 'GET':
         car_requests = Car_requests.objects.filter(status='В ожидании проверки')
@@ -51,9 +56,32 @@ def bb_index(request):
         context = {'car_requests': car_requests,}
         return HttpResponse(template.render(context, request))
 
+
+@login_required
+def accepted(request):
+    if request.method == 'GET':
+        car_requests = Car_requests.objects.filter(status='Одобрить')
+        template = loader.get_template('big_brother/index.html')
+        context = {'car_requests': car_requests,}
+        return HttpResponse(template.render(context, request))
+
+
+@login_required
+def declined(request):
+    if request.method == 'GET':
+        car_requests = Car_requests.objects.filter(status='Отклонить')
+        template = loader.get_template('big_brother/index.html')
+        context = {'car_requests': car_requests,}
+        return HttpResponse(template.render(context, request))
+
+
 @login_required
 def req_card(request, req_id):
     car_request = Car_requests.objects.get(id=req_id)
+    try:
+        car_pass = Car_passes.objects.get(req_id=req_id)
+    except Car_passes.DoesNotExist:
+        car_pass = 'DoesNotExist'
     template = loader.get_template('big_brother/req_card.html')
-    context = {'req_id': req_id, 'car_request': car_request,}
+    context = {'req_id': req_id, 'car_request': car_request, 'car_pass': car_pass}
     return HttpResponse(template.render(context, request))
