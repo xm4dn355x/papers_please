b78c8265deefcd74eeddb152defdd29eca3f369b
m4dn355 <xm4dn355x@gmail.com>
2020-05-12 13:32:09 +0500

    DEBUG mode Returns

--- papers/papers/settings.py
+++ papers/papers/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '192.168.0.1', '192.168.0.111', '212.220.15.188', ]
 
